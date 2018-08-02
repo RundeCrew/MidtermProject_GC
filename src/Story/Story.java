cdb10227b8fdd96ba76ada5322b6e2badd84feeb
standen <baileyb7@yahoo.com>
2018-08-02 10:19:55 -0400

    added first line

--- src/Story/Story.java
+++ src/Story/Story.java
@@ -2,4 +2,8 @@ package Story;
 
 public class Story {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+	}
 }
