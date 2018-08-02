1090bbe64646253b5e561e5ac05ec2b96a36f245
standen <baileyb7@yahoo.com>
2018-08-02 16:37:44 -0400

    adding to app work flow

--- src/midterm/SVKPartyStoreApp.java
+++ src/midterm/SVKPartyStoreApp.java
@@ -1,29 +1,64 @@
 package midterm;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class SVKPartyStoreApp {
 
 	public static void main(String[] args) {
-		// Welcome guest
-		// Get name?
 		
 		Scanner scnr = new Scanner(System.in);
 		SVKUtilFile.createBlankFile("inventory.txt");
 		List<Inventory> inventory = SVKUtilFile.readFile();
-		for (Inventory inv : inventory) {
-			System.out.println(inv.getProductName() + "\t" + inv.getPrice());
-		}
+		String cont = null;
+		
 		// Present today's products in an Array
-		// DO/WHILE
-			// Ask what product they want to start with?
-			// Call ShoppingCart method
-			// Asks if they want to add anything else?
-			// Option to print original product list again any time
-		// When exiting call CheckOut for total 
-			// then call Checkout.payment
-		//give option to email reciept
+				// DO/WHILE
+					// Ask what product they want to start with?
+					// Call ShoppingCart method
+					// Asks if they want to add anything else?
+					// Option to print original product list again any time
+				// When exiting call CheckOut for total 
+					// then call Checkout.payment
+				//give option to email reciept
+		
+		System.out.println("Welcome to SVK Party Store!");
+		System.out.println("Southeast Michigan's Gourmet Party Store");
+		
+		// String guestName = Validator.getString(scnr, "What is your name?");
+		// System.out.println("Thanks " + guestName + ", today our selection of gourmet products is:");
+		int itemSKU = 0;
+		
+		int i = 1;
+		System.out.println("SKU\tProduct\t\t\tPrice");
+		System.out.println("===\t=======\t\t\t======");
+		
+		for (Inventory inv : inventory) {
+			System.out.println(i++ + ".\t" + inv.getProductName() + "\t\t\t" + inv.getPrice());
+		}
+		
+//		do {
+//			System.out.println("What would you like to add to your shopping cart (enter SKU number)?");
+//			// itemSKU = shoppingcart method
+//			// prints out udpated cart with current subtotal
+//			System.out.println("\nDo you want to add anything else ('y') "
+//					+ "review products ('options') or checkout ('exit')?");
+//			cont = scnr.next();
+//				if (cont.equals("options")) {
+//					printInventory(inventory);
+//					System.out.println("\nDo you want to add anything else ('y') "
+//							+ "review products ('options') or checkout ('exit')?");
+//				}
+//			
+//		} while (cont.equals('y'));
+			
+		
 	}
+	
+	public static void printInventory(Inventory inventory) {
+		
+		return;
+	}
+	
 }
