d2b0e854d0fff9c71f0e179add90d4bf82a6a794
standen <baileyb7@yahoo.com>
2018-08-02 10:27:24 -0400

    added birthday line

--- src/Story/Story.java
+++ src/Story/Story.java
@@ -7,6 +7,7 @@ public class Story {
 		System.out.println("Once upon a time in the mystical land of Shangrila "
 				+ "there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys."
 				+ "On the second Saturday, Kai-Yun headed out to gather berries."
-				+ "Because he felt the urge to make a pie for his party that night.");
+				+ "Because he felt the urge to make a pie for his party that night."
+				+ "Kai-Yun had a birthday that day and wanted to celebrate with all the ninjas.");
 	}
 }
