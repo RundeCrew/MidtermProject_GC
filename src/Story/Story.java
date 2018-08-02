c06f9bc94f72608de5aa2cf0a637101bbd6a1394
RundeCrew <kris.runde@gmail.com>
2018-08-02 10:23:35 -0400

    added 2nd line

--- src/Story/Story.java
+++ src/Story/Story.java
@@ -4,6 +4,8 @@ public class Story {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Shangrila "
+				+ "there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys."
+				+ "On the second Saturday, Kai-Yun headed out to gather berries.");
 	}
 }
