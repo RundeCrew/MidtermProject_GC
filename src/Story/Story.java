665a0121c354d208fb17c3c5d216231c0b60768a
RundeCrew <kris.runde@gmail.com>
2018-08-02 10:31:36 -0400

    nextline

--- src/Story/Story.java
+++ src/Story/Story.java
@@ -8,6 +8,8 @@ public class Story {
 				+ "there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys."
 				+ "On the second Saturday, Kai-Yun headed out to gather berries."
 				+ "Because he felt the urge to make a pie for his party that night."
-				+ "Kai-Yun had a birthday that day and wanted to celebrate with all the ninjas.");
+				+ "Kai-Yun had a birthday that day and wanted to celebrate with all the ninjas."
+				+ "He forgot to send out invitations though so he had to slack everyone before he headed out!");
+
 	}
 }
