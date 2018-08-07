ac4d95415d8b814344eb5732c3336ab25af0dc6a
virginia <mavirginia.cordero@gmail.com>
2018-08-06 22:29:50 -0400

    Added EmailClient.java file, and as example, the SVKPartyStoreApp class is sending an email when you run the project. This is just a demo.

---
+++ src/midterm/EmailClient.java
@@ -0,0 +1,69 @@
+package midterm;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+     	
+public class EmailClient {
+	
+	public EmailClient() {
+		
+	}
+	
+	public void sendEmail(String recipient) {
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+				
+		Session session = Session.getInstance(props, new Authenticator() {
+		    @Override
+		    protected PasswordAuthentication getPasswordAuthentication() {
+		    	String username = "svkpartystore.sales@gmail.com";
+		    	String password = "KrisStan12.3";
+		        return new PasswordAuthentication(username, password);
+		    }
+		});
+		
+		Message message = new MimeMessage(session);
+		
+		try {
+			message.setFrom(new InternetAddress("svkpartystore.sales@gmail.com"));
+			message.setRecipients(
+			Message.RecipientType.TO, InternetAddress.parse(recipient));
+			message.setSubject("Email Client Test");
+			 
+			String msg = "This is my first email using JavaMailer";
+			 
+			MimeBodyPart mimeBodyPart = new MimeBodyPart();
+			mimeBodyPart.setContent(msg, "text/html");
+			 
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(mimeBodyPart);
+			 
+			message.setContent(multipart);
+			 
+			Transport.send(message);
+			System.out.print("Email Sent!!");
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+}
--- src/midterm/SVKPartyStoreApp.java
+++ src/midterm/SVKPartyStoreApp.java
@@ -10,6 +10,9 @@ public class SVKPartyStoreApp {
 
 	public static void main(String[] args) throws ParseException {
 		
+		EmailClient emailClient = new EmailClient();
+		emailClient.sendEmail("mavirginia.cordero@gmail.com");
+		
 		Scanner scnr = new Scanner(System.in);				
 		
 		String cont = "y";
@@ -24,6 +27,7 @@ public class SVKPartyStoreApp {
 		System.out.println("Please enter your name:");
 		String userName = scnr.next();
 		scnr.nextLine();
+		// CheckOut.provideReceipt(scnr, userName);
 		
 		System.out.println("Hello " + userName + ", here is a list of the items we currently have in stock:");
 		
@@ -40,24 +44,25 @@ public class SVKPartyStoreApp {
 			printCart(cart);
 			
 			cont = Validator.checkContinue(scnr, "\nWould you like to add another item (yes or no) or see the list again (list)?");
+//			System.out.println("\nWould you like to add another item to your cart?");
+//			cont = scnr.nextLine();
 			
 		} while (cont.trim().toLowerCase().startsWith("y"));
 		
-		System.out.println(" " + userName + ", here is your final bill: \n ");
-		
+		// System.out.println(" " + userName + ", here is your final order: \n ");
 		double sum = printCart(cart);
+		// CheckOut newtotal = new CheckOut();
 		double grandTotal = CheckOut.calcTaxDeposit(cart, sum);
 		
 		CheckOut.getPaymentMethod(scnr, grandTotal);
 		CheckOut.provideReceipt(scnr, userName);
 		printCart(cart);
-		CheckOut.calcTaxDeposit(cart, sum);
-		
+//		CheckOut.printReceiptSummary(cart, sum);
 
 		System.out.println();
 		
-		System.out.println("Thank you for shopping at the SVK Party Store,\n "
-				+ "\twe appreciate your business.");
+		System.out.println("Thank you for shopping at the SVK Party Store, we appreciate your business.");
+		
 		
 	}
 	
@@ -82,8 +87,7 @@ public class SVKPartyStoreApp {
 		}
 		System.out.println("                              ________________");
 		System.out.printf("%40s", "Subtotal: ");
-		String priceStr = "$" + String.format("%.2f",  sum);
-	    System.out.printf("%6s%n", priceStr);
+		System.out.printf("$%.2f%n",  sum);
 	
 		return sum;
 	}
